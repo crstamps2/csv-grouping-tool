2a8916ac091309c051e6f2ccf87b7cbb8a321e69
Cody Stamps <cody.stamps@hey.com>
2025-08-21 16:01:43 -0400

    add test to make sure rows are returned as array of hashes

--- spec/csv_processor_spec.rb
+++ spec/csv_processor_spec.rb
@@ -7,5 +7,22 @@ describe CSVProcessor do
     it 'should be able to count the number of rows, excluding the header row' do
       expect(subject.rows.count).to eq(8)
     end
+    it 'returns a hash for each row' do
+      rows = subject.rows
+      expect(rows.first).to eq({
+        email: "johns@home.com",
+        firstname: "John",
+        lastname: "Smith",
+        phone: "(555) 123-4567",
+        zip: "94105",
+      })
+      expect(rows.last).to eq({
+        email: nil,
+        firstname: "Josh",
+        lastname: "Smith",
+        phone: nil,
+        zip: "94109",
+      })
+    end
   end
 end
