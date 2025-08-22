1174bcf981ba22c60359d2a36b3dddf7ecfca2f4
Cody Stamps <cody.stamps@hey.com>
2025-08-22 14:48:59 -0400

    refactor to push some logic down into private functions to keep public api more clean

--- services/data_processor.rb
+++ services/data_processor.rb
@@ -1,19 +1,30 @@
 class DataProcessor
   def self.call(data, grouping_method = :email)
-    value_to_id_map = {}
-    id = 0
+    context = {
+      value_to_id_map: {},
+      next_id: 0
+    }
+    
     data.map do |row|
       value = row[grouping_method].to_s
       if value.empty?
-        id += 1
-        {id: id}.merge(row)
-      elsif value_to_id_map.key?(value)
-        {id: value_to_id_map[value]}.merge(row)
+        context[:next_id] += 1
+        {id: context[:next_id]}.merge(row)
       else
-        id += 1
-        value_to_id_map[value] = id
-        {id: id}.merge(row)
+        _find_or_assign_id(value, context).merge(row)
       end
+    end
+  end
+
+  private_class_method
+
+  def self._find_or_assign_id(value, context)
+    if context[:value_to_id_map].key?(value)
+      {id: context[:value_to_id_map][value]}
+    else
+      context[:next_id] += 1
+      context[:value_to_id_map][value] = context[:next_id]
+      {id: context[:next_id]}
     end
   end
 end
