7c94b4019beaee61884f76e86ccfef249a654a12
Cody Stamps <cody.stamps@hey.com>
2025-08-22 15:17:53 -0400

    handle multiple emails on a row

--- services/data_processor.rb
+++ services/data_processor.rb
@@ -6,24 +6,33 @@ class DataProcessor
     }
     
     data.map do |row|
-      value = row[grouping_method].to_s
-      if value.empty?
+      values = _extract_values_for_grouping(row, grouping_method)
+      if values.empty?
         context[:next_id] += 1
         {id: context[:next_id]}.merge(row)
       else
-        _find_or_assign_id(value, context).merge(row)
+        _find_or_assign_id(values, context).merge(row)
       end
     end
   end
 
   private_class_method
 
-  def self._find_or_assign_id(value, context)
-    if context[:value_to_id_map].key?(value)
-      {id: context[:value_to_id_map][value]}
+  def self._extract_values_for_grouping(row, grouping_method)
+    row.filter_map do |key, value|
+      if key.to_s.start_with?(grouping_method.to_s)
+        value.to_s unless value.to_s.empty?
+      end
+    end.uniq
+  end
+
+  def self._find_or_assign_id(values, context)
+    existing_id = values.map { |value| context[:value_to_id_map][value] }.compact.first
+    if existing_id
+      {id: existing_id}
     else
       context[:next_id] += 1
-      context[:value_to_id_map][value] = context[:next_id]
+      values.each { |value| context[:value_to_id_map][value] = context[:next_id] }
       {id: context[:next_id]}
     end
   end
--- spec/services/data_processor_spec.rb
+++ spec/services/data_processor_spec.rb
@@ -25,6 +25,30 @@ describe DataProcessor do
             { id: 4, email: 'johns@home.com', firstname: 'John', lastname: 'Smith', phone: '(555) 867-5309', zip: '12345' },
           ])
       end
+
+      context 'multiple emails per record' do
+        let(:data) {
+          [
+            { email1: '', email2: 'johns3@home.com', firstname: 'John', lastname: 'Smith', phone: '(555) 123-4567', zip: '94105' },
+            { email1: 'charlesch@gmail.com', email2: 'charlesch2@gmail.com', firstname: 'Charles', lastname: 'Chou', phone: '(858) 888-8888', zip: '92064' },
+            { email1: 'joshs@home.com', email2: 'joshs2@home.com', firstname: 'Josh', lastname: 'Smith', phone: nil, zip: '94109' },
+            { email1: 'johns@home.com', email2: '', firstname: 'Jon', lastname: 'Smithery', phone: '(555) 123-4567', zip: '94105' },
+            { email1: '', email2: 'johns@home.com', firstname: 'John', lastname: 'Smith', phone: '(555) 867-5309', zip: '12345' },
+            { email1: 'johns5@home.com', email2: 'johns@home.com', firstname: 'John', lastname: 'Smith', phone: '(555) 867-5309', zip: '12345' },
+          ]
+        }
+
+        it 'should return an array of hashes with id prepended to all hashes, having the same id for the same emails' do
+          expect(subject).to eq([
+            { id: 1, email1: '', email2: 'johns3@home.com', firstname: 'John', lastname: 'Smith', phone: '(555) 123-4567', zip: '94105' },
+            { id: 2, email1: 'charlesch@gmail.com', email2: 'charlesch2@gmail.com', firstname: 'Charles', lastname: 'Chou', phone: '(858) 888-8888', zip: '92064' },
+            { id: 3, email1: 'joshs@home.com', email2: 'joshs2@home.com', firstname: 'Josh', lastname: 'Smith', phone: nil, zip: '94109' },
+            { id: 4, email1: 'johns@home.com', email2: '', firstname: 'Jon', lastname: 'Smithery', phone: '(555) 123-4567', zip: '94105' },
+            { id: 4, email1: '', email2: 'johns@home.com', firstname: 'John', lastname: 'Smith', phone: '(555) 867-5309', zip: '12345' },
+            { id: 4, email1: 'johns5@home.com', email2: 'johns@home.com', firstname: 'John', lastname: 'Smith', phone: '(555) 867-5309', zip: '12345' },
+          ])
+        end
+      end
     end
 
     context 'group by phone' do
