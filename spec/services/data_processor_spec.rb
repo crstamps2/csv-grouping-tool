f8981c0c04b3eb13940793192f06425cfca15e2b
Cody Stamps <cody.stamps@hey.com>
2025-08-22 11:41:55 -0400

    add test to cover when some emails are empty

--- spec/services/data_processor_spec.rb
+++ spec/services/data_processor_spec.rb
@@ -20,5 +20,29 @@ describe DataProcessor do
         { id: 1, email: 'johns@home.com', firstname: 'Jon', lastname: 'Smithery', phone: '(555) 123-4567', zip: '94105' },
       ])
     end
+
+    context 'when the data has some empty emails' do
+      let(:data) {
+        [
+          { email: '', firstname: 'John', lastname: 'Smith', phone: '(555) 123-4567', zip: '94105' },
+          { email: 'charlesch@gmail.com', firstname: 'Charles', lastname: 'Chou', phone: '(858) 888-8888', zip: '92064' },
+          { email: 'joshs@home.com', firstname: 'Josh', lastname: 'Smith', phone: nil, zip: '94109' },
+          { email: 'johns@home.com', firstname: 'Jon', lastname: 'Smithery', phone: '(555) 123-4567', zip: '94105' },
+          { email: '', firstname: 'John', lastname: 'Smith', phone: '(555) 867-5309', zip: '12345' },
+          { email: 'johns@home.com', firstname: 'John', lastname: 'Smith', phone: '(555) 867-5309', zip: '12345' },
+        ]
+      }
+
+      it 'should return an array of hashes with id prepended to all hashes, having the same id for the same emails' do
+        expect(subject).to eq([
+          { id: 1, email: '', firstname: 'John', lastname: 'Smith', phone: '(555) 123-4567', zip: '94105' },
+          { id: 2, email: 'charlesch@gmail.com', firstname: 'Charles', lastname: 'Chou', phone: '(858) 888-8888', zip: '92064' },
+          { id: 3, email: 'joshs@home.com', firstname: 'Josh', lastname: 'Smith', phone: nil, zip: '94109' },
+          { id: 4, email: 'johns@home.com', firstname: 'Jon', lastname: 'Smithery', phone: '(555) 123-4567', zip: '94105' },
+          { id: 5, email: '', firstname: 'John', lastname: 'Smith', phone: '(555) 867-5309', zip: '12345' },
+          { id: 4, email: 'johns@home.com', firstname: 'John', lastname: 'Smith', phone: '(555) 867-5309', zip: '12345' },
+        ])
+      end
+    end
   end
 end
